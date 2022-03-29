2949c9a3329a56f621721c660dcf77d6acde16db
ProjectWalkingTree <102138681+ProjectWalkingTree@users.noreply.github.com>
2022-03-29 14:45:24 +0530

    Delete CarRentalSystemProject directory

--- CarRentalSystemProject/CarRentalSystem/src/main/java/com/CarRentalSystem/Interface/Rentalrepo.java
+++
@@ -1,9 +0,0 @@
-package com.CarRentalSystem.Interface;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.CarRentalSystem.Entites.Rental;
-
-public interface Rentalrepo extends JpaRepository<Rental, Integer>{
-
-}
