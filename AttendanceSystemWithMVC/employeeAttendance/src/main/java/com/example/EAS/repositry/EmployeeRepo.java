c357706afbebbec607eda8a8470022a466b79177
ProjectWalkingTree <102138681+ProjectWalkingTree@users.noreply.github.com>
2022-04-04 12:59:17 +0530

    Delete AttendanceSystemWithMVC directory

--- AttendanceSystemWithMVC/employeeAttendance/src/main/java/com/example/EAS/repositry/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.EAS.repositry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.EAS.entity.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-
-}
